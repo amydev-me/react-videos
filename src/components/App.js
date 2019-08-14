d1d9cde97e281ae0e6cc2f0fb6922470bb390da1
Amy Pyae Phyo Naing <amy@lfuturedev.com>
2019-08-14 14:37:30 +0630

    VideoDetail

--- src/components/App.js
+++ src/components/App.js
@@ -9,18 +9,25 @@ class App extends React.Component{
 
     state = { videos: [] , selectedVideo:null}
 
+    componentDidMount(){
+        this.onTermSubmit('buildings');
+    }
 
     onTermSubmit = async term => {
         const response = await axios.get('https://www.googleapis.com/youtube/v3/search',{
             params:{
                 part: 'snippet',
                 maxResults:5,
+                type:'video',
                 key : KEY,
                 q: term
              }
         });
 
-        this.setState({ videos: response.data.items});
+        this.setState({ 
+            videos: response.data.items,
+            selectedVideo : response.data.items[0]
+        });
     };
 
     onVideoSelect = video =>{
@@ -31,11 +38,19 @@ class App extends React.Component{
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList 
-                    onVideoSelect={this.onVideoSelect} 
-                    videos={this.state.videos}
-                />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList 
+                                onVideoSelect={this.onVideoSelect} 
+                                videos={this.state.videos}
+                            />
+                        </div>
+                    </div>
+                </div>                
             </div>
         );
     }
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,11 +3,19 @@ import VideoItem from './VideoItem';
 
 const VideoList = ({ videos, onVideoSelect }) => {
 
-    const renderedList = videos.map((video) => {
-                            return <VideoItem onVideoSelect={onVideoSelect} video={video}/>;
-                        });
+    const renderedList = videos.map(video => {
+        let _key = video.id.videoId;
+        return (
+            <VideoItem 
+                key={_key}
+                onVideoSelect={onVideoSelect} 
+                video={video}
+                />
+        );
+        
+    });
 
-    return <div className="ui list">{renderedList}</div>
+    return <div className="ui relaxed divided list">{renderedList}</div>
 }
 
 
